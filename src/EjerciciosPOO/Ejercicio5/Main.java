f254de83ce2f50b057305e77fbadd2932d31b33c
aargarjua <aargarjua@alu.edu.gva.es>
2025-02-06 14:49:16 +0100

    Ejercicios Aaron

--- src/EjerciciosPOO/Ejercicio5/Main.java
+++ src/EjerciciosPOO/Ejercicio5/Main.java
@@ -4,10 +4,27 @@ public class Main {
     public static void main(String[] args) {
 
         Banco santander = new Banco(1, "Santander");
-        Sucursal sucursalesS = new Sucursal(11, "Castellon", santander);
-        santander.añadirSucursal(sucursalesS);
-        Cliente aaron = new Cliente(22, "Aaron");
-        Prestec paaron = new Prestec(1, 5000, aaron, sucursalesS);
-        aaron.añadirPrestamo(paaron);
+
+            Sucursal sucursalesS = new Sucursal(11, "Castellon", santander);
+
+                santander.añadirSucursal(sucursalesS);
+
+            Cliente aaron = new Cliente(22, "Aaron");
+
+            Prestec paaron = new Prestec(1, 5000, aaron, sucursalesS);
+
+            CuentaCorriente cuentaAaron = new CuentaCorriente(1,"Aaron", sucursalesS);
+
+            Domiciliacion domiciliacion = new Domiciliacion(1,cuentaAaron);
+
+                aaron.añadirPrestamo(paaron);
+
+                aaron.añadirCuentas(cuentaAaron);
+
+                cuentaAaron.añadirCliente(aaron);
+
+                cuentaAaron.añadirDomiciliacion(domiciliacion);
+
+
     }
 }
